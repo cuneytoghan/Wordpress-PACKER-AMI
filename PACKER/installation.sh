588766a5155216f371ff84d3b2080b38e05ff613
Cuneyt Oghan <coghann@gmail.com>
2023-04-06 05:54:54 +0000

    added .gitignore and installation.sh

---
+++ PACKER/installation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#installation packer for amazon linux docs 
+# https://developer.hashicorp.com/packer/downloads
+sudo yum install -y yum-utils shadow-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install packer
