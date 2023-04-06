20743116c11b0dc63a8ae758d58935643abea0d3
Cuneyt Oghan <coghann@gmail.com>
2023-04-06 06:00:25 +0000

    added documantation

--- PACKER/installation.sh
+++ PACKER/installation.sh
@@ -5,3 +5,5 @@
 sudo yum install -y yum-utils shadow-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install packer
+
+# learn from documantation https://developer.hashicorp.com/packer/tutorials
